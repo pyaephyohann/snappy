55e4fbfeea2f09ed2d6edcd1ae17202c7d19237c
Pyae Phyo Han <pyaephyohan2200@gmail.com>
2026-08-10 21:18:51 +0700

    feat: database design

--- prisma.config.ts
+++ prisma.config.ts
@@ -4,6 +4,6 @@ import { defineConfig } from "prisma/config";
 export default defineConfig({
   schema: "prisma/schema.prisma",
   datasource: {
-    url: process.env.DATABASE_URL || "postgresql://user:password@localhost:5432/snappy",
+    url: process.env.DATABASE_URL,
   },
 });
